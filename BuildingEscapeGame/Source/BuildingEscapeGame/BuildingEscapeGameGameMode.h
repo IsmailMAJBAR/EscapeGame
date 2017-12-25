76051ab41326674091a69df5b2a9a73caa9adeaa
inital Commit <macme@macMes-MacBook-Pro.local>
2017-12-25 14:11:42 +0100

    no message

---
+++ BuildingEscapeGame/Source/BuildingEscapeGame/BuildingEscapeGameGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "BuildingEscapeGameGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGESCAPEGAME_API ABuildingEscapeGameGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
